43d7da4b822891acbaf78ac5b5c7def7eb079512
Rade473 <r.shekularats@gmail.com>
2023-08-01 15:51:11 +0200

    New task form started

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import "./style.css";
+const tx = document.getElementsByTagName("textarea");
+for (let i = 0; i < tx.length; i++) {
+  tx[i].setAttribute("style", "height:" + tx[i].scrollHeight + "px;");
+  tx[i].addEventListener("input", OnInput, false);
+}
+
+function OnInput() {
+  this.style.height = 0;
+  this.style.height = this.scrollHeight + "px";
+}
